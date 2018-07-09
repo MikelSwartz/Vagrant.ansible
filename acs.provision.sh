466d621d136357fde26ce8e77043550f41e24d1c
Mike <mikelswartz@gmail.com>
2018-07-09 10:15:10 -0400

    Added code to install the latest 2.7 version of python to get around an SSL certificate error in python 2.7.6

--- acs.provision.sh
+++ acs.provision.sh
@@ -1,4 +1,5 @@
 apt-get install software-properties-common -y
 apt-add-repository ppa:ansible/ansible -y
+add-apt-repository ppa:jonathonf/python-2.7 -y #Installs the latest 2.7 version of python to get around an SSL certificate error
 apt-get update
-apt-get install ansible vim sshpass tree -y
+apt-get install ansible vim sshpass tree python2.7 -y
